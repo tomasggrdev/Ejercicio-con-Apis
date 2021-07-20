16a2e3f506e1fd956f20a516d9bd8f0e9b36801d
Tomas Gonzalez <tomasggrdev@gmail.com>
2021-07-20 18:24:13 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Api_url = "https://jsonplaceholder.typicode.com";
+
+const HTMLResponse = document.querySelector("#app");
+
+fetch(`${Api_url}/users`)
+.then(response => response.json())
+.then(users => {
+    const tpl = users.map(user => `<li>${user.name} ${user.email}</li>`);
+    HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
+})
+.then(response =>console.log(response))
+// const xhr = new XMLHttpRequest();
+
+// function onrequest() {
+//     if(this.readyState === 200 && this.status===4) {
+//         console.log(this.response);
+//     }
+// }
+
+// xhr.addEventListener("load",onrequest)
+// xhr.open("GET",`${Api_url}/users/1`)
+// xhr.send()
